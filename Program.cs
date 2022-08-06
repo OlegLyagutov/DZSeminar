f8ee7acba55671b1507a6cbc7c44b0899c533b14
Oleg <o.lyagutov@gmail.com>
2022-08-07 00:08:28 +0300

    add zadacha6

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Console.Write("Введите число 1: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// int secendNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber > secendNumber)
+// {
+//     Console.WriteLine(firstNumber + " является max");
+// }
+// else
+// {
+//     Console.WriteLine(secendNumber + " является max");
+// }
+
+
+// Console.Write("Введите число 1: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// int secendNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 3: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+// int max = firstNumber;
+// if (secendNumber > max) max = secendNumber;
+// if (thirdNumber > max) max = thirdNumber;
+// Console.WriteLine(max + " является max");
+
+
